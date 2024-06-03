a78583b5925eb8a067d33c18bf732488f6359ba3
nociza <azicon@berkeley.edu>
2024-06-02 18:47:22 -0700

    done with hotpotqa

---
+++ HotpotQA/download_dataset.sh
@@ -0,0 +1,2 @@
+wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_fullwiki_v1.json -P data/hotpotqa-fullwiki/
+wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_train_v1.1.json -P data/hotpotqa-fullwiki/
