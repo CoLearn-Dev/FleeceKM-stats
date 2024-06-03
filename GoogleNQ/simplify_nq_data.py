137f706164e5b127f93ed0b2ba0cbb89a8ff2a47
nociza <azicon@berkeley.edu>
2024-06-02 22:51:20 -0700

    Refactor text_utils.py and simplify_nq_data.py

--- GoogleNQ/simplify_nq_data.py
+++ GoogleNQ/simplify_nq_data.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 # Copyright 2018 The Google AI Language Team Authors.
 #
+# Modifications Copyright 2024 nociza.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -12,6 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 r"""Script to apply `text_utils.simplify_nq_data` to all examples in a split.
 
 We have provided the processed training set at the link below.
--- GoogleNQ/text_utils.py
+++ GoogleNQ/text_utils.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 # Copyright 2018 The Google AI Language Team Authors.
 #
+# Modifications Copyright 2024 nociza.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -12,6 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 r"""Utilities to simplify the canonical NQ data.
 
 The canonical NQ data contains the HTML of each Wikipedia page along with a
